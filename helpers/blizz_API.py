1a4f06012876e3b9a0e973985b7656ff1d8dcd63
Luis E <pitufo89@gmail.com>
2021-03-18 15:54:59 +0100

    ADD token request method.

--- helpers/blizz_API.py
+++ helpers/blizz_API.py
@@ -1,8 +1,23 @@
 import os
+import requests
+import json
 from dotenv import load_dotenv
 
 load_dotenv()
 
-CLIENT = os.getenv('BZ_OAUTH_CLIENT')
-TOKEN = os.getenv('BZ_OAUTH_TOKEN')
+CLIENT = os.getenv("BZ_OAUTH_CLIENT")
+TOKEN = os.getenv("BZ_OAUTH_TOKEN")
 
+
+def get_access_token():
+    url = "https://us.battle.net/oauth/token"
+    data = {"grant_type": "client_credentials"}
+    r = call_api(url, data)
+    if r.status_code == 200:
+        response = json.loads(r.content)
+        return response["access_token"]
+    return False
+
+
+def call_api(url, data):
+    return requests.post(url, auth=(CLIENT, TOKEN), data=data)
