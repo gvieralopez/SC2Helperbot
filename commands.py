090fc9f64597dda9615d1ba9ed371d5e91dc9afe
Gustavo Viera López <gvieralopez@gmail.com>
2021-03-26 00:41:20 +0100

    Add docstring of process command function

--- commands.py
+++ commands.py
@@ -1,4 +1,14 @@
 
 
 def process_command(command, tgid):
+    """
+    Identify the command into the available commands and execute it.
+
+    Args:
+        command (str): The command to execute
+        tgid (int): Telegram id of the user
+
+    Returns:
+        str: Result of executing the command
+    """
     return command
