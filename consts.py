44f60bcdd0bfcc3847e20c7459881030f7bbed77
Gustavo Viera López <gvieralopez@gmail.com>
2022-02-03 17:40:04 +0100

    Add constants

---
+++ consts.py
@@ -0,0 +1,30 @@
+race_emojis = {
+    'zerg' : '🪲',
+    'protoss' : '✨',
+    'terran' : '🧯',
+    'random' : '❓'
+    }
+
+league_emojis = {
+    'Bronze' : '🥉',
+    'Silver' : '🥈',
+    'Gold' : '🥇',
+    'Platinum' : '⚓',
+    'Diamond' : '💎',
+    'Master' : '🎖️'
+}
+
+region_emojis = {
+    1 : "🇺🇸",
+    2 : "🇪🇺",
+    3 : "🇰🇷",
+    4 : "🇹🇼"
+}
+
+region = {
+    1 : "US",
+    2 : "EU",
+    3 : "KR",
+    4 : "TW"
+}
+
