ddd81931deb3184f8d8b12fa980074deea030aae
Gustavo Viera López <gvieralopez@gmail.com>
2022-04-03 18:54:29 +0200

    Update scripts with new import path and APIs

--- scripts/fetch_player_data.py
+++ scripts/fetch_player_data.py
@@ -1,13 +1,14 @@
 from sys import argv
 
-from sc2bot.client.blizzard import resolvers
+from sc2bot.client import resolvers
 from sc2bot.database.schema import Player
 
 
 def main(region_id: int, profile_id: int, resolver_index: int):
     resolver = resolvers[resolver_index]
-    player = Player(region_id=region_id, profile_id=profile_id, display_name="")
-    print(resolver.resolve_player_display_name(player), resolver.resolve_player_stats(player))
+    name = resolver.resolve_player_display_name(region_id=region_id, profile_id=profile_id)
+    player = Player(region_id=region_id, profile_id=profile_id, display_name=name)
+    print(name, resolver.resolve_player_stats(player))
 
 
 if __name__ == "__main__":
