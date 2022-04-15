171f4210fb220c47a361262662add1f753104a56
Silvio Delgado <silvio.delgado@tetrel.ai>
2022-04-15 15:53:14 +0200

    Implement main entrypoint

---
+++ sc2bot/__main__.py
@@ -0,0 +1,50 @@
+import logging
+import os
+
+from aiogram import Bot, Dispatcher, executor, types
+
+from sc2bot.bot_response import BotResponse
+from sc2bot.routes import routes
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+bot = Bot(token=os.environ.get("BOT_TOKEN"))
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(state="*")
+async def router(message: types.Message):
+    if not message.is_command():
+        return
+
+    command_name, *arguments = extract_command(message.text)
+
+    if command_name not in routes:
+        return
+
+    try:
+        telegram_id = int(message["from"]["id"])
+        response = routes[command_name].target(telegram_id, *arguments)
+    except Exception as e:
+        logger.warning(f"There was an error executing the command: {e}")
+        response = routes[command_name].help
+
+    await answer(message, response)
+
+
+async def answer(message: types.Message, response: BotResponse):
+    if response.image:
+        await message.answer_photo(response.image, response.text, parse_mode="html")
+        return
+
+    await message.answer(response.text, parse_mode="html")
+
+
+def extract_command(text: str) -> list[str]:
+    raw_without_slash = text[1:]
+    return raw_without_slash.split(" ")
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
--- sc2bot/routes.py
+++ sc2bot/routes.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 from typing import Callable
 
-from sc2bot.bot_response import BotResponse
+from sc2bot.bot_response import BotResponse, render
 from sc2bot.controller import add_new_player_stats, set_battle_tag, add_player, retrieve_user
 from sc2bot.commands import register, link, user, fetch
 
@@ -10,11 +10,14 @@ from sc2bot.commands import register, link, user, fetch
 class Route:
     name: str
     target: Callable[..., BotResponse]
+    help: BotResponse
 
 
-commands = [
-    Route(register, set_battle_tag),
-    Route(link, add_player),
-    Route(user, retrieve_user),
-    Route(fetch, add_new_player_stats),
+_routes = [
+    Route(register, set_battle_tag, render("help")),
+    Route(link, add_player, render("help")),
+    Route(user, retrieve_user, render("help")),
+    Route(fetch, add_new_player_stats, render("help")),
 ]
+
+routes = {route.name: route for route in _routes}
